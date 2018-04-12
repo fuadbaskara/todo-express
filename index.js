871a8de47f958697eb94b3c6f26bfe79bff5579c
Fuad Baskara P <fuadbaskara@gmail.com>
2018-04-12 14:47:16 +0700

    first push

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+
+
+
+let todoList = [{
+    todo: "learn Express",
+    done: true
+  },
+  {
+    todo: "learn React",
+    done: false
+  }
+]
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/todo", (req, res) => {
+  res.send({data: todoList});
+});
+
+app.get("/todo/:id", (req, res) => {
+  let length = todoList.length;
+  let index = req.params.id;
+  if (index < length) {
+    res.send({ data: todoList[index] })
+  } else {
+    res.send("data not found");;
+  }
+});
+
+app.post("/todo", (req, res) => {
+  let todo = req.body.todo;
+  let done = JSON.parse(req.body.done);
+  console.log(typeof todo);
+  if (todo === "") {
+    res.send("todo data is empty");
+  } else {
+    newTodo = {
+      todo: todo,
+      done: done
+    }
+    todoList.push(newTodo);
+    res.send({data: newTodo})
+  }
+});
+
+app.delete("/todo/:id", (req, res) => {
+  let length = todoList.length;
+  let index = req.params.id;
+
+  if (index < length) {
+    todoList.splice(index, 1);
+    res.send({ success: true, data: todoList });
+  } else {
+    res.send({ sucess: false, message: "data not found" });
+  }
+});
+
+app.get("/todo/search", (req, res) => {
+  let search = req.query.todo;
+  let result = todoList.filter(e =>
+    e.todo.toLowerCase().includes(search.toLowerCase())
+  );
+  res.send({ success: true, result: result });
+});
+
+app.listen(3000, () =>
+  console.log("example app running on port 3000"));
